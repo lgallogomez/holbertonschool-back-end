46f846f74575a6dc93ee4f3430fd35afa2e39ce2
lgallogomez <l.gallogomez@gmail.com>
2024-01-17 14:09:28 -0500

    corrected 1st exercise

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -4,28 +4,34 @@
 if __name__ == "__main__":
     import requests
     from sys import argv
-    import json
+    import json   
 
+    """getting info from todos api"""
     user_id = argv[1]
-
-    api_url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
-
-    api_to_dos = "https://jsonplaceholder.typicode.com/todos/"
-
-    to_do_response = requests.get(api_to_dos)
+    api_todos = f"https://jsonplaceholder.typicode.com/users/{user_id}/todos"
+    to_do_response = requests.get(api_todos)
     json_to_do = to_do_response.json()
-    print(json_to_do)
+   
+    
 
-    list = []
-    for item in json_to_do:
-        if item["userId"] == user_id:
-            print(item)
-
-    user_id_int = int(user_id)
-
+    """getting user from users api"""
+    api_url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
     response = requests.get(api_url)
-    dicti_json = response.json()
-    print(response)
-    print(type(dicti_json))
-    print(dicti_json)
-    print(f"Employee {dicti_json['name']} is done with ")
+    dicti_json = response.json() 
+    
+    tasks_done = 0
+    all_tasks = 0
+    for item in json_to_do:
+        if item["completed"] is True:
+            tasks_done += 1
+
+    for item in json_to_do:
+        all_tasks += 1
+
+    print(f"Employee {dicti_json['name']} is done with({tasks_done}/{all_tasks}):")
+    """printing  """
+    
+    for item in json_to_do:
+        if item["completed"] is True:
+            print(item["title"])
+    
