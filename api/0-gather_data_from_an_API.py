bed2a93afac90e3ab405600de31dd580587d7e17
lgallogomez <l.gallogomez@gmail.com>
2024-01-17 14:17:00 -0500

    corrected 1st exercise

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""module gathers data using api and displays info"""
+"""
+module gathers data using api and displays info
+"""
 
 if __name__ == "__main__":
     import requests
